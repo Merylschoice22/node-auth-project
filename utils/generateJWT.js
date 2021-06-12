0124bae21de950acf15e8563b057037572b98406
merylschoice22 <kekrieg.digital@gmail.com>
2021-06-12 14:58:43 +0200

    Updated generateJWT

--- utils/generateJWT.js
+++ utils/generateJWT.js
@@ -1,15 +1,13 @@
-const jwt = require("jsonwebtoken");
+const jsonwebtoken = require("jsonwebtoken");
 require("dotenv").config(); // here we use dotenv module which we installed in the begining to access environment variables from .env file
 
-function generateJWT(user_id) {
-  // payload is just an object which usually contains some information about user but not confidential information such as password.
-  const payload = {
-    user: {
-      id: user_id,
-    },
-  };
-
-  return jwt.sign(payload, process.env.jwtSecret, { expiresIn: "1h" });
+// take an id, calculate and return a signed JWT
+function generateJWT(userId) {
+  const secret = process.env.JWT_SECRET;
+  const jwt = jsonwebtoken.sign({ id: userId }, secret);
+  return jwt;
 }
 
-module.exports = generateJWT; // we export this function to use it inside routes/user.js
+module.exports = {
+  generateJWT: generateJWT,
+};
